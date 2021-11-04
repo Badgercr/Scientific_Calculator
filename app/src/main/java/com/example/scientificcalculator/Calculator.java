efd03e183105b718152cdc3343e3f969477c3ac6
Chris Rodriguez <rodriguezchris024@gmail.com>
2021-11-04 00:03:48 -0700

    changes

--- app/src/main/java/com/example/scientificcalculator/Calculator.java
+++ app/src/main/java/com/example/scientificcalculator/Calculator.java
@@ -1,13 +1,14 @@
 package com.example.scientificcalculator;
 
+import android.view.View;
 import android.widget.TextView;
 import java.text.DecimalFormat;
 
 
 public class Calculator {
     //two numbers used in operation
-    double firstNum;
-    double lastNum;
+    Double firstNum;
+    Double lastNum;
     //operation variable
     int operation; //no operation = -1
     //actual text space, showing numbers
@@ -17,12 +18,165 @@ public class Calculator {
     //constructor
     public Calculator(TextView workspace){
         this.workspace = workspace;
-        firstNum = 0;
-        lastNum = 0;
+        df = new DecimalFormat("##########.#######");
         this.workspace.setText("0");
     }
     //----------------------------functions----------------------------//
+    public void input0(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("0");
+        }
+        else
+        {
+            workspace.append("0");
+        }
+    }
+    public void input1(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("1");
+        }
+        else{
+            workspace.append("1");
+        }
 
+    }
+    public void input2(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("2");
+        }
+        else{
+            workspace.append("2");
+        }
+
+    }
+    public void input3(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("3");
+        }
+        else {
+            workspace.append("3");
+        }
+    }
+    public void input4(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("4");
+        }
+        else {
+            workspace.append("4");
+        }
+    }
+    public void input5(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("5");
+        }
+        else {
+            workspace.append("5");
+        }
+    }
+    public void input6(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("6");
+        }
+        else {
+            workspace.append("6");
+        }
+    }
+    public void input7(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("7");
+        }
+        else {
+            workspace.append("7");
+        }
+    }
+    public void input8(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("8");
+        }
+        else {
+            workspace.append("8");
+        }
+    }
+    public void input9(){
+        if (workspace.length() == 9){
+            return;
+        }
+        else if (workspace.length() == 1 & workspace.getText().toString().equals("0")){
+            workspace.setText("9");
+        }
+        else {
+            workspace.append("9");
+        }
+    }
+    public void clear(){
+        workspace.setText("0");
+
+    }
+    public void inputDecimal(){
+        if (workspace.length() >= 7 | workspace.getText().toString().contains("."))
+        {
+            return;
+        }
+        else{
+            workspace.append(".");
+        }
+    }
+    public void inputSign(){
+        if (workspace.getText().toString().contains("-")){
+            workspace.setText(workspace.getText().toString().substring(1));
+        }
+        else {
+            workspace.setText("-" + workspace.getText().toString());
+        }
+    }
+    public void inputAddition(){
+        operation = 1;
+        if (firstNum == null){
+            firstNum = Double.valueOf(workspace.getText().toString());
+        }
+        else{
+            lastNum = Double.valueOf(workspace.getText().toString());
+        }
+
+    }
+    public void inputEqual(){
+        double result;
+        workspace.setText("hey");
+        /*switch (operation){
+            case 1:
+                result = firstNum + lastNum;
+                firstNum = result;
+                //df.format(result);
+                workspace.setText("hey");
+        }*/
+    }
 
 
 
@@ -30,8 +184,25 @@ public class Calculator {
     public void setWorkspace(TextView workspace) {
         this.workspace = workspace;
     }
+
+    public void setFirstNum(double firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public void setLastNum(double lastNum) {
+        this.lastNum = lastNum;
+    }
+
     //------------------------getters-----------------------------------------//
     public TextView getWorkspace() {
         return workspace;
+    }
+
+    public double getFirstNum() {
+        return firstNum;
+    }
+
+    public double getLastNum() {
+        return lastNum;
     }
 }
--- app/src/main/java/com/example/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/example/scientificcalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.scientificcalculator;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,8 +19,82 @@ public class MainActivity extends AppCompatActivity {
         /*
         Initialize the workspace your calc instance
         */
+        findViewById(R.id.clear).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.clear();
+            }
+        });
+        findViewById(R.id.zero).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input0();
+            }
+        });
+        findViewById(R.id.one).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input1();
+            }
+        });
+        findViewById(R.id.two).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input2();
+            }
+        });
+        findViewById(R.id.three).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input3();
+            }
+        });
+        findViewById(R.id.four).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input4();
+            }
+        });
+        findViewById(R.id.five).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input5();
+            }
+        });
+        findViewById(R.id.six).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input6();
+            }
+        });
+        findViewById(R.id.seven).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input7();
+            }
+        });
+        findViewById(R.id.eighth).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input8();
+            }
+        });
 
-
+        findViewById(R.id.nine).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.input9();
+            }
+        });
+        findViewById(R.id.decimal_point).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.inputDecimal();
+            }
+        });
+        findViewById(R.id.negative).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.inputSign();
+            }
+        });
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.inputAddition();
+            }
+        });
+        findViewById(R.id.equal).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                calc.inputEqual();
+            }
+        });
 
 
 
@@ -35,4 +110,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 }
